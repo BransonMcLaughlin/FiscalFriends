8e17d0a06999dce679f0d21e58f0bb01c5113551
BransonMcLaughlin <mclaughlbw@jacks.sfasu.edu>
2024-03-06 13:59:27 -0600

    Add project files.

---
+++ FiscalFriendsDB/dbo/Tables/Budget.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Budget] (
+    [BudgetID]    INT          NOT NULL,
+    [Userid]      INT          NOT NULL,
+    [TotalBudget] DECIMAL (18) NOT NULL,
+    CONSTRAINT [PK_Budget] PRIMARY KEY CLUSTERED ([BudgetID] ASC)
+);
+
---
+++ FiscalFriendsDB/dbo/Tables/Income.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Income] (
+    [Incomeid]       INT          NOT NULL,
+    [Userid]         INT          NOT NULL,
+    [IncomeSource]   VARCHAR (50) NOT NULL,
+    [IncomeAmount]   DECIMAL (18) NOT NULL,
+    [IncomeInterval] DECIMAL (18) NOT NULL,
+    [NetPayday]      DATE         NOT NULL,
+    [Recurring]      VARCHAR (3)  NOT NULL,
+    CONSTRAINT [PK_Income] PRIMARY KEY CLUSTERED ([Incomeid] ASC),
+    CONSTRAINT [FK_Income_Income2] FOREIGN KEY ([Userid]) REFERENCES [dbo].[Income] ([Incomeid])
+);
+
