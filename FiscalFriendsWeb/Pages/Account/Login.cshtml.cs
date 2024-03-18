b9399d357aca8a42ae294f077cbec6622441552c
BransonMcLaughlin <mclaughlbw@jacks.sfasu.edu>
2024-03-18 14:15:27 -0500

    Login page

---
+++ FiscalFriendsWeb/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FiscalFriendsWeb.Pages.Account
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost() 
+        { 
+        }
+    }
+}
