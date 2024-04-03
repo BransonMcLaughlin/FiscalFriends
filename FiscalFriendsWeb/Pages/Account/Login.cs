c02136356c2dfeb6f66484a9cf4deebf22e41bd8
lambertal1 <lambertal1@jacks.sfasu.edu>
2024-04-03 13:31:39 -0500

    Updated Login

--- FiscalFriendsWeb/Pages/Account/Login.cs
+++ FiscalFriendsWeb/Pages/Account/Login.cs
@@ -10,10 +10,7 @@ namespace FiscalFriendsWeb.Pages.Account
     {
         [BindProperty]
         public Login loginUser { get; set; }
-        //public Login() 
-        //{
 
-        //}
         public void OnGet()
         {
 
@@ -23,8 +20,31 @@ namespace FiscalFriendsWeb.Pages.Account
         {
             if (ModelState.IsValid)
             {
-                SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString());
-                String cmdText = "SELECT PasswordHash FROM [User] WHERE Username=@username;";
+                // check login credentials
+                if(ValidateCredentials())
+                {
+                    //if the credentials are valid redirect user to profile
+                    return RedirectToPage("Profile");
+                }
+                else
+                {
+                    //else, display error
+                    ModelState.AddModelError("LoginError", "Invalid Credentials, Try again.");
+                    return Page();
+                }
+                
+            }
+            else
+            {
+                return Page();
+            }
+        } // end ActionResult
+
+        private bool ValidateCredentials()
+        {
+            using (SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
+            {
+                String cmdText = "SELECT PasswordHash,PersonID FROM [User] WHERE Username=@username;";
                 SqlCommand cmd = new SqlCommand(cmdText, conn);
                 cmd.Parameters.AddWithValue("@username", loginUser.Username);
                 conn.Open();
@@ -37,26 +57,38 @@ namespace FiscalFriendsWeb.Pages.Account
                         string passwordHash = reader.GetString(0);
                         if (SecurityHelper.VerifyPassword(loginUser.Password, passwordHash))
                         {
-                            return RedirectToPage("Profile");
+                            int personId = reader.GetInt32(1);
+                            UpdatePersonLogInTime(personId);
+                            return true;
                         }
                         else
                         {
-                            ModelState.AddModelError("LoginError", "Invalid Credentials, Try again.");
-                            return Page();
+                            return false;
                         }
+                    }
+                    else
+                    {
+                        return false;
                     }
                 }
                 else
                 {
-                    ModelState.AddModelError("LoginError", "Invalid credentails. Try again.");
+                    return false;
                 }
-                conn.Close();
-                return RedirectToPage();
             }
-            else
+        }
+
+        private void UpdatePersonLogInTime(int personId)
+        {
+            using(SqlConnection conn = new SqlConnection(SecurityHelper.GetDBConnectionString()))
             {
-                return Page();
+                string cmdText = "UPDATE [User] Set LastLoggedIn=@LastLoggedIn WHERE PersonId=@personId";
+                SqlCommand cmd = new SqlCommand( cmdText, conn);
+                cmd.Parameters.AddWithValue("@LastLoggedIn", DateTime.Now);
+                cmd.Parameters.AddWithValue("@personId", personId);
+                conn.Open();
+                cmd.ExecuteNonQuery();
             }
-        } // end ActionResult
+        }
     } // end class 
 } // end namespace 
