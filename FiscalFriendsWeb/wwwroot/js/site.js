fa7bf3ddc6bef246d8f5da90120f4160f6adce16
Luis Ovalle <ovallele@jacks.sfasu.edu>
2024-04-10 12:31:02 -0500

    Worked on the layout files, added logo, javascript for language dropdown and other stuff.

--- FiscalFriendsWeb/wwwroot/js/site.js
+++ FiscalFriendsWeb/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function changeLanguage(language) {
+
+    document.getElementById('currentLanguage').textContent = language;
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var languageItems = document.querySelectorAll('.dropdown-item');
+    languageItems.forEach(function (item) {
+
+        item.addEventListener('click', function () {
+
+            changeLanguage(this.textContent);
+
+        });
+
+    });
+
+});
