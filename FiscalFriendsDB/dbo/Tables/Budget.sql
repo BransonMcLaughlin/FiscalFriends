1085d567d2a95fa7d3a3ad6a050da61265cfcc68
BransonMcLaughlin <mclaughlbw@jacks.sfasu.edu>
2024-04-11 13:42:39 -0500

    had to edit budget.sql

--- FiscalFriendsDB/dbo/Tables/Budget.sql
+++ FiscalFriendsDB/dbo/Tables/Budget.sql
@@ -2,6 +2,10 @@
     [BudgetID]    INT          NOT NULL,
     [Userid]      INT          NOT NULL,
     [TotalBudget] DECIMAL (18) NOT NULL,
+    [StartDate]   DATE         NOT NULL,
+    [EndDate]     DATE         NOT NULL,
     CONSTRAINT [PK_Budget] PRIMARY KEY CLUSTERED ([BudgetID] ASC)
 );
+
+
 
