cbd0432cae6bfb98777db21cee13c0a1866c2877
lambertal1 <lambertal1@jacks.sfasu.edu>
2024-04-15 07:56:52 -0500

    Started on View Daily Expenses

--- FiscalFriendsWeb/Pages/DailyExpenses/ViewDailyExpenses.cshtml.cs
+++ FiscalFriendsWeb/Pages/DailyExpenses/ViewDailyExpenses.cshtml.cs
@@ -1,10 +1,17 @@
+using FiscalFriendsWeb.Pages.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FiscalFriendsWeb.Pages.DailyExpenses
 {
+    [BindProperties]
     public class ViewDailyExpensesModel : PageModel
     {
+
+        public List<SelectListItem> Categories { get; set; } = new List<SelectListItem>();
+        public List<DailyExpense> DailyExpenses { get; set; } = new List<DailyExpense>();
+        public int selectedCategory;
         public void OnGet()
         {
         }
