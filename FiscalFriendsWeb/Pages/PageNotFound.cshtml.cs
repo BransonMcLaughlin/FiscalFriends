ca4dd27c16043cf4742cc61abb4ea4e28b436adc
lambertal1 <lambertal1@jacks.sfasu.edu>
2024-05-10 16:44:59 -0500

    Moved password reset incase luis cannot fix style

---
+++ FiscalFriendsWeb/Pages/PageNotFound.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FiscalFriendsWeb.Pages
+{
+    public class PageNotFoundModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
