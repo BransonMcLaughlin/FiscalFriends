a1846ac6e506d5c6ba049152fe78b786c4638f06
lambertal1 <lambertal1@jacks.sfasu.edu>
2024-05-10 10:33:43 -0500

    Updated Populate DB Script

--- FiscalFriendsDB/dbo/PopulateDB.sql
+++ FiscalFriendsDB/dbo/PopulateDB.sql
@@ -11,17 +11,27 @@ Post-Deployment Script Template
 */
 SET IDENTITY_INSERT [dbo].[ExpenseCategories] ON 
 GO
-INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (1, N'Fast Food')
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (1, N'Fast Food                                                                                           ')
 GO
-INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (2, N'Groceries')
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (2, N'Groceries                                                                                           ')
 GO
-INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (3, N'Misc.')
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (3, N'Housing                                                                                             ')
+GO
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (4, N'Healthcare                                                                                          ')
+GO
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (5, N'Utilities                                                                                           ')
+GO
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (6, N'Entertainment                                                                                       ')
+GO
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (7, N'Education                                                                                           ')
+GO
+INSERT [dbo].[ExpenseCategories] ([CategoryId], [CategoryDescription]) VALUES (8, N'Miscellaneous                                                                                       ')
 GO
 SET IDENTITY_INSERT [dbo].[ExpenseCategories] OFF
 GO
 SET IDENTITY_INSERT [dbo].[User] ON 
 GO
-INSERT [dbo].[User] ([PersonID], [FirstName], [LastName], [Email], [PhoneNumber], [UserName], [PasswordHash], [LastLoggedIn], [Birthday], [AccountMade]) VALUES (1, N'Andrew', N'Lambert', N'Andrew2020Lambert@gmail.com', N'18324973965', N'Lambertal1', N'$2a$13$j7/oIxHHX32XRUGYfWRhXOyhqoN0Vt2DyYAojtpraWRDdaDS3x6AG', CAST(N'2024-04-15T10:01:43.747' AS DateTime), CAST(N'2024-04-24' AS Date), CAST(N'2024-04-15T08:26:31.000' AS DateTime))
+INSERT [dbo].[User] ([PersonID], [FirstName], [LastName], [Email], [PhoneNumber], [UserName], [PasswordHash], [LastLoggedIn], [Birthday], [AccountMade]) VALUES (1, N'Root', N'User', N'Root@FiscalFriends.com', N'1111111111', N'Root', N'$2a$13$Xop1oGt8cuZYGl9QJOBRY.F0CPJFRYhqVizoZcwlAi9H51UYXdMiu', CAST(N'2024-05-10T10:05:55.223' AS DateTime), CAST(N'2024-05-10' AS Date), CAST(N'2024-05-10T10:05:38.000' AS DateTime))
 GO
 SET IDENTITY_INSERT [dbo].[User] OFF
 GO
